f762239930ea1496d9fc9e2e40244fe8b3bf1385
Jellybin <pjbboseung0817@gmail.com>
2022-10-13 20:03:03 +0900

    22-10-13

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    Lorem ipsum dolor sit amet,
+    consectetur adipisicing elita
+  </body>
+</html>
